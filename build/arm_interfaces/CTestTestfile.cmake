d7c34c295e0cd6fe7bac5b97f756cc6bb84fe16d
Mark Mckessock <mark.mckessock@gmail.com>
2022-10-15 18:09:46 +0000

    Remove build/install directories

--- build/arm_interfaces/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Ros2-Arm-2022/src/arm_interfaces
-# Build directory: /home/ubuntu/Ros2-Arm-2022/build/arm_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("arm_interfaces__py")
