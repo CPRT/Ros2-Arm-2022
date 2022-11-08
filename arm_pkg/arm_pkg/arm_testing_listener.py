10ce6adb46f917744c94080d1c21b5d911bfe994
Erik <erikcaell@gmail.com>
2022-11-08 10:48:09 -0500

    Logging encoder data
    Threw the encoder data into a csv file so I can make a graph to prove the code is working once I run it on the roboclaw.

--- arm_pkg/arm_pkg/arm_testing_listener.py
+++ arm_pkg/arm_pkg/arm_testing_listener.py
@@ -1,3 +1,4 @@
+from time import time
 import rclpy
 from rclpy.node import Node
 
@@ -6,6 +7,10 @@ from geometry_msgs.msg import Pose
 
 from arm_interfaces.msg import LiveTune
 from arm_interfaces.msg import SixFloats
+
+import time
+import csv
+from datetime import datetime
 
 class ArmTestingListener(Node):
 
@@ -20,6 +25,28 @@ class ArmTestingListener(Node):
         self.loopInterval = 0.01
         self.timeSinceStartup = 0
         self.timeSinceNewData = 0
+
+        self.startTime = time.time()
+
+        csvFilePath = "/home/erik/Downloads/ArmNodeTesting-LoggingFiles_FAll2022/"
+        csvFilename = "Logging_" + datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+
+        try:
+            self.csvFile = open(csvFilePath + csvFilename, 'w')
+        except:
+            print("FIled to file the folder: " + csvFilePath)
+            try:
+                print("Opening csv where this file is located")
+                self.csvFile = open(csvFilename, 'w')
+            except:
+                exit
+
+
+        self.csvWriter = csv.writer(self.csvFile)
+
+        csvRow = (  f"TIME BLANK RAD m0 m1 m2 m3 m4 m5 BLANK RAW m0 m1 m2 m3 m4 m5").split()
+        self.csvWriter.writerow(csvRow)
+        
 
         # Setup subscribers
         self.encoder_radians = self.create_subscription(
@@ -40,6 +67,9 @@ class ArmTestingListener(Node):
         self.timeSinceNewData = 0
 
     def loopRun(self):
+        measuredTime = time.time() - self.startTime
+        timeDifference = measuredTime - self.timeSinceStartup
+
         display = ( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
                     "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
                     "Encoder values in radians: \n"
@@ -59,13 +89,36 @@ class ArmTestingListener(Node):
                     f"m5: {self.encoderRaw.m5:.5f} \n\n\n"
 
                     f"Time since new data {self.timeSinceNewData:.3f} seconds \n"
-                    f"Time since listener startup: {self.timeSinceStartup:.3f} seconds "
+                    f"Time since listener startup: {self.timeSinceStartup:.3f} seconds \n"
+                    f"Measured time: {measuredTime:.3f} \n"
+                    f"Difference: {timeDifference:.3f} \n"
+
         )
 
         print(display)
 
         self.timeSinceStartup += self.loopInterval
         self.timeSinceNewData += self.loopInterval
+
+        csvRow = [  measuredTime,
+                    " ",
+                    "RAD",
+                    self.angles.m0,
+                    self.angles.m1,
+                    self.angles.m2,
+                    self.angles.m3,
+                    self.angles.m4,
+                    self.angles.m5,
+                    " ",
+                    "RAW",
+                    self.encoderRaw.m0,
+                    self.encoderRaw.m1,
+                    self.encoderRaw.m2,
+                    self.encoderRaw.m3,
+                    self.encoderRaw.m4,
+                    self.encoderRaw.m5,]
+
+        self.csvWriter.writerow(csvRow)
 
 
                 
@@ -74,6 +127,10 @@ def main(args=None):
     rclpy.init(args=args)
     node = ArmTestingListener()
     rclpy.spin(node)
+
+    # Close the csv
+    node.csvFile.close()
+
     node.destroy_node()
     rclpy.shutdown()
     
